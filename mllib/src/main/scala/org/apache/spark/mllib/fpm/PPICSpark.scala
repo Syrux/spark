e7ae37f19d33ebc6902ca278cb07cbfd6e48af49
Syrux <pokcyril@hotmail.com>
2017-04-20 14:23:54 +0200

    multiArray to test

--- mllib/src/main/scala/org/apache/spark/mllib/fpm/PPICSpark.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/fpm/PPICSpark.scala
@@ -35,7 +35,7 @@ import org.apache.spark.mllib.fpm.PrefixSpan.Postfix
  * @param minSup minimal support for a frequent pattern
  * @param minPatternLength min pattern length for a frequent pattern
  * @param maxPatternLength max pattern length for a frequent pattern
- * @param outPutItemSetPermutation A boolean value that define whether running the algorithm
+ * @param isMultiItemPattern A boolean value that define whether running the algorithm
  *                                 on <(1,2) 3> should output (1,2) 3; 1 3; 2 3 or just the
  *                                 last two ...
  */
@@ -43,22 +43,46 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
                               val minSup: Long,
                               val minPatternLength: Int,
                               val maxPatternLength: Int,
-                              val outPutItemSetPermutation: Boolean)
+                              val isMultiItemPattern: Boolean)
   extends CPModel with App with Logging with Serializable{
 
   // Define separator
   var separator = 0
   var epsilon = -1
 
+  var numItems = 0
   var lenSeqMax = 0
   var nbSequences = 0
 
   var itemSupportCounter: scala.collection.mutable.Map[Int, Int] =
     scala.collection.mutable.Map[Int, Int]()
 
+  // One to one translation map
+  var translationMap = collection.mutable.Map[Int, Int]()
+  var translationMapReverse = collection.mutable.Map[Int, Int]()
+
   def getLastItemFromPrefix(): Array[Int] = {
     prefix.drop(prefix.lastIndexOf(separator) + 1)
   }
+
+  /**
+   * Adds an entry to the translation map.
+   *
+   * @param value : An item supported by the sequences
+   * @return the new representation of the old value, which
+   *         will be used as a substitute for the actual
+   *         value in the algorithm.
+   *         (The new reprensentation is an Int > 0)
+   */
+  def updateTranslationMap(value: Int): Int = {
+
+    // Add new value in reverse map
+    numItems += 1
+    translationMapReverse.update(numItems, value)
+    // Return the new representation
+    numItems
+  }
+
   /**
    * Removes first element before first 0
    * Then count support for each item, so that unsupported item can be removed in the future.
@@ -66,12 +90,15 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
    * @return the inputed postfixes, desencapsulated and with the first elem removed
    */
   def preProcessPostfixes(postfixes: Array[Postfix]):
-    (Array[Array[Int]], Array[ReversibleArrayStack[Set[Int]]]) = {
+    (Array[Array[Int]], Array[ReversibleArrayStack[Set[Int]]],
+      Array[Array[Int]], Array[Array[Int]]) = {
 
     val preprocessedSequence = scala.collection.mutable.ArrayBuilder
       .make[Array[Int]]
     val newPartialProjection = scala.collection.mutable.ArrayBuilder
       .make[ReversibleArrayStack[Set[Int]]]
+    var firstItemPosInSid = scala.collection.mutable.ArrayBuilder.make[Array[Int]]
+    var lastItemPosInSid = scala.collection.mutable.ArrayBuilder.make[Array[Int]]
     itemSupportCounter = collection.mutable.Map[Int, Int]()
 
     // Count support for each item
@@ -88,6 +115,7 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
 
     // Filter unsupported item, make sure there are no consecutive zeros
     itemSupportCounter = itemSupportCounter.retain((k, v) => v >= minSup)
+
     // Remove unsupported item
     for (postfix <- postfixes) {
       // Create SDB postfix
@@ -96,6 +124,8 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
       var nbItemAdded = 0
       val curPostfix = scala.collection.mutable.ArrayBuilder.make[Int]
       val partialProjections = scala.collection.mutable.Stack[Int]()
+      val firstItemPosInSeq = Array.fill(itemSupportCounter.size)(0)
+      val lastItemPosInSeq = Array.fill(itemSupportCounter.size)(0)
 
       for (i <- Range(1, postfix.items.length)) {
         val x = postfix.items(i)
@@ -112,9 +142,16 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
           }
         }
         else if (itemSupportCounter.getOrElse(x, 0) >= minSup) {
+          val trans = translationMap.getOrElseUpdate(x, updateTranslationMap(x))
           lastItemWasEpsilon = false
-          curPostfix += x
           nbItemAdded += 1
+          curPostfix += trans
+          // Update pos lists
+          lastItemPosInSeq(trans) = nbItemAdded
+          // If necessary, update first pos in current sequence for current item
+          if (firstItemPosInSeq(trans) == 0) {
+            firstItemPosInSeq(trans) = nbItemAdded
+          }
         }
         // Correct partial start
         if (postfix.partialStarts.contains(i) && nbItemAdded > 0) {
@@ -133,14 +170,19 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
         val partialProj = new ReversibleArrayStack[Set[Int]](solver, 11)
         partialProj.push(partialProjections.toSet)
         newPartialProjection += partialProj
+        // Append pos lists
+        lastItemPosInSeq(0) = nbItemAdded
+        firstItemPosInSid += firstItemPosInSeq
+        lastItemPosInSid += lastItemPosInSeq
       }
     }
-    (preprocessedSequence.result(), newPartialProjection.result())
+    (preprocessedSequence.result(), newPartialProjection.result(),
+      firstItemPosInSid.result(), lastItemPosInSid.result())
   }
 
   def initCPVariables(maxPatternLength: Int,
                       itemSet : scala.collection.Set[Int],
-                      outPutItemSetPermutation: Boolean):
+                      isMultiItemPattern: Boolean):
     Array[CPIntVar] = {
 
     val nonZeroSet = itemSet -- Set(separator)
@@ -149,19 +191,21 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
     val specialValSet = Set(separator, epsilon)
 
     val maxLength = {
-      if (!outPutItemSetPermutation && maxPatternLength < lenSeqMax) {
+      if (!isMultiItemPattern && maxPatternLength < lenSeqMax) {
         math.min((maxPatternLength*2) + 1, lenSeqMax)
       }
       else lenSeqMax
     }
 
     // TODO : actual min pattern length calculation
-    val minLength = 2
+    val minLength =
+      if (!isMultiItemPattern) 3
+      else 2
 
     val CPVariables = new Array[CPIntVar](maxLength)
     for (i <- CPVariables.indices) {
       CPVariables(i) = {
-        if (outPutItemSetPermutation) {
+        if (isMultiItemPattern) {
           if (i < minLength) {
             CPIntVar.sparse(itemSet)
           }
@@ -191,18 +235,24 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
     epsilon = -1
     lenSeqMax = 0
     itemSupportCounter = scala.collection.mutable.Map[Int, Int]()
+    // Init translation maps
+    translationMap = collection.mutable.Map[Int, Int]()
+    translationMapReverse = collection.mutable.Map[Int, Int]()
+    translationMapReverse.put(0, 0)
 
     // INIT sdb
     val preprocessed = preProcessPostfixes(postfixes)
     val sdb = preprocessed._1
     val newPartialProjection = preprocessed._2
+    val firstPosList = preprocessed._3
+    val lastPosList = preprocessed._4
     // IF we only have epsilon and separator, no need to search
     if(sdb.length < minSup) return Iterator()
 
-    val itemSet = itemSupportCounter.keySet
+    val itemSet = translationMapReverse.keySet
 
     // Init CPvariables
-    val CPVariables = initCPVariables(maxPatternLength, itemSet, outPutItemSetPermutation)
+    val CPVariables = initCPVariables(maxPatternLength, itemSet, isMultiItemPattern)
 
     // Setup max pattern length
     val amongItem = (itemSet -- Set(separator)).toSet
@@ -210,6 +260,8 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
       add(atMost(maxPatternLength, CPVariables, amongItem))
     }
 
+    // Get last item in prefix, doesn't need to be translated to work, we don't check the value
+    // only use their count
     val lastPrefixItemElems = getLastItemFromPrefix()
 
     // PRINT
@@ -225,6 +277,10 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
     sdb.foreach(x => {x.foreach(x=> {print(x); print(" ")}); println()})
     println("new partial")
     newPartialProjection.foreach(x => {x.top.foreach(x=> {print(x); print(" ")}); println()})
+    println("firstItemPosInSid")
+    firstPosList.foreach(x=> {x.foreach(x=> {print(x); print(" ")}); println()})
+    println("lastItemPosInSid")
+    lastPosList.foreach(x=> {x.foreach(x=> {print(x); print(" ")}); println()})
     println("itemSupportCounter")
     for(elem <- itemSet){
       println(elem + " : " + itemSupportCounter.getOrElse(elem, 0))
@@ -240,7 +296,8 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
     // RUN
 
     val solutions = scala.collection.mutable.ArrayBuffer.empty[(Array[Int], Long)]
-    val c = new sparkCP(CPVariables, sdb, newPartialProjection, lastPrefixItemElems, minSup)
+    val c = new sparkCP(CPVariables, sdb, newPartialProjection, lastPrefixItemElems, minSup,
+      isMultiItemPattern, firstPosList, lastPosList)
     try {
       add(c)
     } catch {
@@ -251,7 +308,9 @@ private[fpm] class PPICSpark( val prefix: Array[Int],
     this.solver.onSolution {
       val curSol = scala.collection.mutable.ArrayBuffer.empty[Int]
       CPVariables.foreach(x => {
-        if (x.value != epsilon)curSol += x.value
+        if (x.value != epsilon) {
+          curSol += translationMapReverse.getOrElse(x.value, -1)
+        }
       })
       solutions += ((curSol.toArray, c.curPrefixSupport))
     }
@@ -267,7 +326,10 @@ class sparkCP(val P: Array[CPIntVar],
               val SDB: Array[Array[Int]],
               val partialProjection: Array[ReversibleArrayStack[Set[Int]]],
               val prefixOfLastItem: Array[Int],
-              val minsup : Long)
+              val minsup : Long,
+              val lookingForMultiItemPatterns : Boolean,
+              val firstPosList : Array[Array[Int]],
+              val lastPosList : Array[Array[Int]])
   extends Constraint(P(0).store, "PPIC-SPARK") {
 
   // Define separator
@@ -282,6 +344,9 @@ class sparkCP(val P: Array[CPIntVar],
 
   // Array with start position in each sequence
   private[this] val posList = Array.fill[ReversibleInt](SDB.length)(new ReversibleInt(s, 0))
+
+  // Map containing unsupported elements
+  private[this] val supportMap = scala.collection.mutable.Map.empty[Int, Int]
 
   /**
    * Entry in constraint, function for all init
@@ -344,6 +409,18 @@ class sparkCP(val P: Array[CPIntVar],
 
       // Clean next V
       if(v < P.length - 1) {
+        // Remove unsupported item in next V
+        if (!supportMap.isEmpty) {
+          for (value <- P(v + 1)) {
+            if (value != separator &&
+              value != epsilon &&
+              supportMap.getOrElse(value, 0) < minsup) {
+
+              if (P(v + 1).removeValue(value) == Failure) return Failure
+            }
+          }
+          supportMap.clear()
+        }
         // IF v != separator, v+1 != epsilon
         if (P(v).value != separator) {
           if (P(v + 1).removeValue(epsilon) == Failure) return Failure
@@ -414,33 +491,53 @@ class sparkCP(val P: Array[CPIntVar],
       var i = index.value
       // Get cursequence
       val curSeq = SDB(sequenceIndex)
+      // Get cur postlists
+      val firstPos = firstPosList(sequenceIndex)
+      val lastPos = lastPosList(sequenceIndex)
 
       // Project prefix
-      if (soughtItem == separator) {
-        // If we look for a separator, find next one
+      if (i >= lastPos(soughtItem)) {
+        // No need to look for that item, not present
+        i = curSeq.length
+      }
+      else if (soughtItem == separator) {
+        // If we look for a separator, simply find next one
         while (i < curSeq.length && curSeq(i) != separator) i += 1
         // Update partial Proj
         partialProjection(sequenceIndex).push(Set.empty[Int])
+        // Update item support
+        for (item <- lastPos.indices) {
+          if (lastPos(item) >= i) supportMap.update(item, supportMap.getOrElse(item, 0) + 1)
+        }
       }
       else if (checkElemOfP(v - 1, separator)) {
         // If last elem was a separator, find projection anywhere
         val newPartialProj = collection.mutable.Set.empty[Int]
         // Special case for first item
-        while (i < curSeq.length && curSeq(i) != soughtItem) i += 1
-        if (i < curSeq.length) {
-          // Add partial proj to cur ig necessary
+        if (i < firstPos(soughtItem)) i = firstPos(soughtItem) - 1
+        else while (i < lastPos(soughtItem) && curSeq(i) != soughtItem) i += 1
+        // Find all other items
+        if (i < curSeq.length && lookingForMultiItemPatterns) {
+          // Add partial proj to cur if necessary
           if (curSeq(i + 1) != separator) newPartialProj += i + 1
           var checker = i + 1
+          var shouldUpdateItemSupport = true
           // Search partial projections
-          while (checker < curSeq.length) {
+          while (checker < lastPos(soughtItem)) {
+            if (shouldUpdateItemSupport) {
+              if (curSeq(checker) == separator) shouldUpdateItemSupport = false
+              else supportMap.update(curSeq(checker), supportMap.getOrElse(curSeq(checker), 0) + 1)
+            }
             if (curSeq(checker) == soughtItem && curSeq(checker + 1) != separator) {
               // Partial proj found, add it
               newPartialProj += checker + 1
+              shouldUpdateItemSupport = true
             }
             checker += 1
           }
         }
         partialProjection(sequenceIndex).push(newPartialProj.toSet)
+        // Don't update item support
       }
       else {
         // Else, search only partial projections
@@ -449,7 +546,7 @@ class sparkCP(val P: Array[CPIntVar],
         for (position <- partialProj) {
           var checker = position
           // Find searched item
-          while (checker < curSeq.length &&
+          while (checker < lastPos(soughtItem) &&
             curSeq(checker) != separator &&
             curSeq(checker) != soughtItem) checker += 1
 
@@ -458,7 +555,13 @@ class sparkCP(val P: Array[CPIntVar],
             supported = true
             // If found and extensible, add to new partial proj.
             if (curSeq(checker + 1) != separator) {
-              newPartialProj += (checker + 1)
+              checker += 1
+              newPartialProj += (checker)
+              // Update support map
+              while (curSeq(checker) != separator) {
+                supportMap.update(curSeq(checker), supportMap.getOrElse(curSeq(checker), 0) + 1)
+                checker += 1
+              }
             }
           }
         }
@@ -466,6 +569,7 @@ class sparkCP(val P: Array[CPIntVar],
         partialProjection(sequenceIndex).push(newPartialProj.toSet)
         // IF not supported, put i to end of sequence
         if (!supported) i = curSeq.length
+        // Don't update item support
       }
 
       // Update start position in sequence
